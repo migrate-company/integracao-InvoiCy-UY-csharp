5c2acb348572871af491add7d747051d2989fb01
Eduardo Arruda <eduhza@gmail.com>
2023-07-26 11:46:08 -0300

    First Commit
    Empty Console App

---
+++ IntegracionInvoicy/IntegracionInvoicy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IntegracionInvoicy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
